750ccb5b980aabcdad22b19598d272b234869aef
Ali Kösemen <98844648+Alikosemen@users.noreply.github.com>
2024-02-07 14:53:29 +0300

    Update SQLConnection.cs

--- Hospital Management and Appointment System Automation/SQLConnection.cs
+++ Hospital Management and Appointment System Automation/SQLConnection.cs
@@ -10,7 +10,7 @@ namespace Hospital_Management_and_Appointment_System_Automation
     {
         public SqlConnection sql_connection()
         {
-            SqlConnection connect = new SqlConnection("Data Source=DESKTOP-320STE0\\;Initial Catalog=\"Hospital Management and Appointment System Automation\";Integrated Security=True");
+            SqlConnection connect = new SqlConnection("Data Source=Your-MSSQL-Server-Name\\;Initial Catalog=\"Hospital Management and Appointment System Automation\";Integrated Security=True");
             connect.Open();
             return connect;
         }
